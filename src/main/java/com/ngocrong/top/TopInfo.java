9194609eab47fa9733febe9000473ca0fbfd3329
NQMP123 <108941743+NQMP123@users.noreply.github.com>
2025-06-28 11:15:39 +0700

    Load leaderboards from database

---
+++ src/main/java/com/ngocrong/top/TopInfo.java
@@ -0,0 +1,13 @@
+package com.ngocrong.top;
+
+public class TopInfo {
+    public int playerID;
+    public String name;
+    public long score;
+    public short head;
+    public short body;
+    public short leg;
+    public String info;
+    public String info2;
+    public int rank;
+}
